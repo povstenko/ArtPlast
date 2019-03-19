9496542e1e0cd3e07d2180072fffd64e50ad5c5e
povstenko <v.povstenko@gmail.com>
2019-03-19 21:25:41 +0200

    CollapseExpand Demo project

---
+++ OrderDemo/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace OrderDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.p1 = new System.Windows.Forms.Panel();
+            this.p2 = new System.Windows.Forms.Panel();
+            this.p3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.Menu;
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.FlatAppearance.BorderSize = 0;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn1.Image = ((System.Drawing.Image)(resources.GetObject("btn1.Image")));
+            this.btn1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn1.Location = new System.Drawing.Point(12, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(267, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Tag = "0";
+            this.btn1.Text = "Frame";
+            this.btn1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.Menu;
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn2.Image = ((System.Drawing.Image)(resources.GetObject("btn2.Image")));
+            this.btn2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn2.Location = new System.Drawing.Point(12, 147);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(267, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Tag = "0";
+            this.btn2.Text = "Glass";
+            this.btn2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.Menu;
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.FlatAppearance.BorderSize = 0;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn3.Image = ((System.Drawing.Image)(resources.GetObject("btn3.Image")));
+            this.btn3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn3.Location = new System.Drawing.Point(12, 282);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(267, 23);
+            this.btn3.TabIndex = 2;
+            this.btn3.Tag = "0";
+            this.btn3.Text = "Furniture";
+            this.btn3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.p1.Location = new System.Drawing.Point(12, 41);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(267, 100);
+            this.p1.TabIndex = 3;
+            this.p1.Visible = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.p2.Location = new System.Drawing.Point(12, 176);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(267, 100);
+            this.p2.TabIndex = 4;
+            this.p2.Visible = false;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.p3.Location = new System.Drawing.Point(12, 311);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(267, 100);
+            this.p3.TabIndex = 5;
+            this.p3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(612, 509);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Panel p1;
+        private System.Windows.Forms.Panel p2;
+        private System.Windows.Forms.Panel p3;
+    }
+}
+
---
+++ OrderDemo/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderDemo
+{
+    public partial class Form1 : Form
+    {
+        const int offset = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ExpandCollapseSettings();
+        }
+
+        private void ExpandCollapseSettings()
+        {
+            btn1.Tag = 0;
+            btn2.Tag = 0;
+            btn3.Tag = 0;
+
+            btn1.Top = offset;
+            btn2.Top = btn1.Top + btn1.Height + offset;
+            btn3.Top = btn2.Top + btn2.Height + offset;
+
+            p1.Width = btn1.Width;
+            p2.Width = btn2.Width;
+            p3.Width = btn3.Width;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((int)btn1.Tag == 0)
+            {
+                // expand
+                btn1.Image = global::OrderDemo.Properties.Resources.icons8_collapse_arrow_24;
+                p1.Top = btn1.Bottom;
+                p1.Visible = true;
+
+                btn2.Top = p1.Bottom + offset;
+                p2.Top = btn2.Bottom;
+
+                if ((int)btn2.Tag == 0)
+                    btn3.Top = btn2.Bottom + offset;
+                else
+                    btn3.Top = p2.Bottom + offset;
+
+                p3.Top = btn3.Bottom;
+
+                btn1.Tag = 1;
+            }
+            else
+            {
+                // hide
+                btn1.Image = global::OrderDemo.Properties.Resources.icons8_expand_arrow_24;
+                p1.Visible = false;
+
+                btn2.Top = btn1.Bottom + offset;
+                p2.Top = btn2.Bottom;
+
+                if ((int)btn2.Tag == 0)
+                    btn3.Top = btn2.Bottom + offset;
+                else
+                    btn3.Top = p2.Bottom + offset;
+
+                p3.Top = btn3.Bottom;
+
+                btn1.Tag = 0;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if ((int)btn2.Tag == 0)
+            {
+                // expand
+                btn2.Image = global::OrderDemo.Properties.Resources.icons8_collapse_arrow_24;
+                p2.Top = btn2.Bottom;
+                p2.Visible = true;
+
+                btn3.Top = p2.Bottom + offset;
+                p3.Top = btn3.Bottom;
+
+                btn2.Tag = 1;
+            }
+            else
+            {
+                // hide
+                btn2.Image = global::OrderDemo.Properties.Resources.icons8_expand_arrow_24;
+                p2.Visible = false;
+
+                btn3.Top = btn2.Bottom + offset;
+                p3.Top = btn3.Bottom;
+
+                btn2.Tag = 0;
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if ((int)btn3.Tag == 0)
+            {
+                // expand
+                btn3.Image = global::OrderDemo.Properties.Resources.icons8_collapse_arrow_24;
+                p3.Top = btn3.Bottom;
+                p3.Visible = true;
+
+                btn3.Tag = 1;
+            }
+            else
+            {
+                // hide
+                btn3.Image = global::OrderDemo.Properties.Resources.icons8_expand_arrow_24;
+                p3.Visible = false;
+
+                btn3.Tag = 0;
+            }
+        }
+    }
+}
