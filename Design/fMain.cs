b53e5d635a09e5d6076055ff7a3227b4865b82f7
povstenko <v.povstenko@gmail.com>
2019-03-19 11:18:28 +0200

    Design template

---
+++ Design/fMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Design
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+            pSide.Height = button1.Height;
+            pSide.Top = button1.Top;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
